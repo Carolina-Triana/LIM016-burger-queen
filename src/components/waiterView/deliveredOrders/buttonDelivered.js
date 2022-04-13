931327323466da7575c1ce970c96bd03d308bf03
G9D4 <gldt2494@gmail.com>
2022-04-13 17:24:01 -0500

    arregla bugs de refactorizacion

--- src/components/waiterView/deliveredOrders/buttonDelivered.js
+++ src/components/waiterView/deliveredOrders/buttonDelivered.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './buttonDelivered.css';
 
-const ButtonOrderDelivered = ({ buttonHandler, text }) => {
+const ButtonOrderDelivered = ({ onClick, text }) => {
   return (
 
       <section className="component-father-DerDelivered">
         <div className= 'component-flex-DerDelivered'>
-          <button className="buttonOrder-DerDelivered" onClick={buttonHandler}>
+          <button className="buttonOrder-DerDelivered" onClick={onClick}>
             <img src={process.env.PUBLIC_URL + '/icons/checked.png'} alt={'checked'} />
             <br></br>
-             {text}
+            {text}
           </button>
         </div>
       </section>
